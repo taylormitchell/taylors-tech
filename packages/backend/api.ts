4beea331706d50ee9bbd34b2ee02692d4d133379
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-05-28 16:21:06 +0200

    process mutations in separate transactions

--- packages/backend/api.ts
+++ packages/backend/api.ts
@@ -59,51 +59,18 @@ export const handler: APIGatewayProxyHandlerV2 = async (evt) => {
           body: e.message,
         };
       }
-
-      // return {
-      //   statusCode: 200,
-      //   body: JSON.stringify({
-      //     lastMutationIDChanges: {},
-      //     cookie: 42,
-      //     patch: [
-      //       { op: "clear" },
-      //       {
-      //         op: "put",
-      //         key: "note/qpdgkvpb9ao",
-      //         value: {
-      //           id: "qpdgkvpb9ao",
-      //           title: null,
-      //           body: "hello world",
-      //           created_at: "2021-09-29T21:00:00.000Z",
-      //         },
-      //       },
-      //       {
-      //         op: "put",
-      //         key: "note/qpdgkvpb9ap",
-      //         value: {
-      //           id: "qpdgkvpb9ap",
-      //           title: null,
-      //           body: "goodbye world",
-      //           created_at: "2021-09-29T21:00:00.000Z",
-      //         },
-      //       },
-      //     ],
-      //   }),
-      // };
     }
     case "POST /push": {
       const push: PushRequestV1 = JSON.parse(evt.body || "{}");
       console.log("Processing push", JSON.stringify(push));
       const t0 = Date.now();
       try {
-        await db.transaction(
-          async (t) => {
-            for (const mutation of push.mutations) {
-              await applyMutation(t, mutation, push.clientGroupID);
-            }
-          },
-          { isolationLevel: "repeatable read" }
-        );
+        for (const mutation of push.mutations) {
+          const t1 = Date.now();
+          console.log(`Processing mutation ${mutation.id}`);
+          await applyMutation(mutation, push.clientGroupID);
+          console.log(`Processed mutation ${mutation.id}`, Date.now() - t1);
+        }
       } catch (e) {
         console.error(e);
         return {
@@ -178,75 +145,79 @@ async function processPull(t: Transaction, pull: PullRequestV1): Promise<PullRes
   }
 }
 
-async function applyMutation(t: Transaction, mutation: MutationV1, clientGroupId: string) {
-  const t1 = Date.now();
-  if (!isMutatorName(mutation.name)) {
-    throw new Error(`Unknown mutation: ${mutation.name}`);
-  }
+async function applyMutation(mutation: MutationV1, clientGroupId: string) {
+  return db.transaction(
+    async (t) => {
+      if (!isMutatorName(mutation.name)) {
+        throw new Error(`Unknown mutation: ${mutation.name}`);
+      }
 
-  const { version: prevVersion } = await t.query.replicacheServer.findFirst({
-    where: eq(replicacheServer.id, serverId),
-  });
-  const nextVersion = prevVersion + 1;
-  const client = await t.query.replicacheClient.findFirst({
-    where: eq(replicacheClient.id, mutation.clientID),
-  });
-  const nextMutationID = (client?.lastMutationID || 0) + 1;
-  console.log("processing mutation", JSON.stringify({ mutation, nextVersion, nextMutationID }));
+      const { version: prevVersion } = await t.query.replicacheServer.findFirst({
+        where: eq(replicacheServer.id, serverId),
+      });
+      const nextVersion = prevVersion + 1;
+      const client = await t.query.replicacheClient.findFirst({
+        where: eq(replicacheClient.id, mutation.clientID),
+      });
+      const nextMutationID = (client?.lastMutationID || 0) + 1;
+      console.log("processing mutation", JSON.stringify({ mutation, nextVersion, nextMutationID }));
 
-  // skip mutations that have already been processed
-  if (mutation.id < nextMutationID) {
-    console.log(`Mutation ${mutation.id} already processed. Skipping.`);
-    return;
-  }
-  // this should never happen
-  if (mutation.id > nextMutationID) {
-    throw new Error(
-      `Mutation ${mutation.id} is from the future - aborting. This can happen in development if the server restarts. In that case, clear appliation data in browser and refresh.`
-    );
-  }
+      // skip mutations that have already been processed
+      if (mutation.id < nextMutationID) {
+        console.log(`Mutation ${mutation.id} already processed. Skipping.`);
+        return;
+      }
+      // this should never happen
+      if (mutation.id > nextMutationID) {
+        throw new Error(
+          `Mutation ${mutation.id} is from the future - aborting. This can happen in development if the server restarts. In that case, clear appliation data in browser and refresh.`
+        );
+      }
 
-  // TODO: can I generate types for these?
-  switch (mutation.name) {
-    case "putNote": {
-      const { id, title, body } = mutation.args as any; // TODO type this
-      await t.insert(note).values({ id, title, body, version: nextVersion });
-      break;
-    }
-    case "updateNote": {
-      const partialNote: Partial<Note> = mutation.args as any; // TODO type this
+      // TODO: can I generate types for these?
+      switch (mutation.name) {
+        case "putNote": {
+          const { id, title, body } = mutation.args as any; // TODO type this
+          await t.insert(note).values({ id, title, body, version: nextVersion });
+          break;
+        }
+        case "updateNote": {
+          const partialNote: Partial<Note> = mutation.args as any; // TODO type this
+          await t
+            .update(note)
+            .set({ ...partialNote, version: nextVersion })
+            .where(eq(note.id, partialNote.id));
+          break;
+        }
+        default: {
+          mutation.name satisfies never;
+          throw new Error(`Unknown mutation: ${mutation.name}`);
+        }
+      }
+
+      // Upsert lastMutationID for requesting client.
+      const result = await t
+        .update(replicacheClient)
+        .set({ lastMutationID: nextMutationID, version: nextVersion })
+        .where(eq(replicacheClient.id, mutation.clientID))
+        .returning();
+      console.log("Upserted lastMutationID", JSON.stringify(result));
+      if (result.length === 0) {
+        console.log("Inserting new client");
+        await t.insert(replicacheClient).values({
+          id: mutation.clientID,
+          clientGroupId,
+          lastMutationID: nextMutationID,
+          version: nextVersion,
+        });
+      }
+
+      // Update global version.
       await t
-        .update(note)
-        .set({ ...partialNote, version: nextVersion })
-        .where(eq(note.id, partialNote.id));
-      break;
-    }
-    default: {
-      mutation.name satisfies never;
-      throw new Error(`Unknown mutation: ${mutation.name}`);
-    }
-  }
-
-  // Upsert lastMutationID for requesting client.
-  const result = await t
-    .update(replicacheClient)
-    .set({ lastMutationID: nextMutationID, version: nextVersion })
-    .where(eq(replicacheClient.id, mutation.clientID))
-    .returning();
-  console.log("Upserted lastMutationID", JSON.stringify(result));
-  if (result.length === 0) {
-    console.log("Inserting new client");
-    await t.insert(replicacheClient).values({
-      id: mutation.clientID,
-      clientGroupId,
-      lastMutationID: nextMutationID,
-      version: nextVersion,
-    });
-  }
-
-  // Update global version.
-  await t
-    .update(replicacheServer)
-    .set({ version: nextVersion })
-    .where(eq(replicacheServer.id, serverId));
+        .update(replicacheServer)
+        .set({ version: nextVersion })
+        .where(eq(replicacheServer.id, serverId));
+    },
+    { isolationLevel: "repeatable read" }
+  );
 }
